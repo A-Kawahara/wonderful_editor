33aebbca892a82fd849344e3b405febc2d35134a
川原杏美 <azu.kawahara@gmail.com>
2023-07-30 10:42:55 +0900

    記事詳細の API とテストの実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,13 @@
 module Api::V1
   class ArticlesController < BaseApiController
     def index
-      @articles = Article.all.order(updated_at: :desc)
-      render json: @articles, each_serializer: Api::V1::ArticlePreviewSerializer
+      articles = Article.all.order(updated_at: :desc)
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    end
+
+    def show
+      article = Article.find(params[:id])
+      render json: article, serializer: Api::V1::ArticleSerializer
     end
   end
 end
--- spec/requests/api/v1/article_request_spec.rb
+++ spec/requests/api/v1/article_request_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Articles", type: :request do
-  describe "GET /articles" do
+  describe "GET / articles" do
     subject { get(api_v1_articles_path) }
 
     let!(:article1) { create(:article, updated_at: 1.days.ago) }
@@ -17,6 +17,37 @@ RSpec.describe "Api::V1::Articles", type: :request do
       expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET /articles/:id" do
+    subject { get(api_v1_article_path(article_id)) }
+
+    context "指定したidの記事が存在するとき" do
+      let(:article) { create(:article) }
+      let(:article_id) { article.id }
+
+      it "その記事が取得できる" do
+        subject
+        res = JSON.parse(response.body)
+
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
+        expect(res.keys).to eq ["id", "title", "body", "updated_at", "user"]
+        expect(res["user"]["id"]).to eq article.user.id
+        expect(res["user"].keys).to eq ["id", "name", "email"]
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "指定したidの記事が存在しないとき" do
+      let(:article_id) { 1000 }
+
+      it "記事が見つからない" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
     end
   end
 end

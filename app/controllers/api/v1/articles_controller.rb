71ebefbbf551cd32d688441e1d5d6f70903a5704
川原杏美 <azu.kawahara@gmail.com>
2023-09-04 11:00:25 +0900

    既存APIの修正

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
   class ArticlesController < BaseApiController
+    skip_before_action :authenticate_user!, only: %i[index show]
+
     def index
       articles = Article.all.order(updated_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
